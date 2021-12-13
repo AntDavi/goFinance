3517088186fb98ef254b830984c29881791010c4
Anthony <ant.davi07@gmail.com>
2021-12-13 16:39:51 -0300

    firts

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {Container, 
+        Header,
+        UserWrapper,
+        UserInfo, 
+        Photo, 
+        User, 
+        UserGreeting, 
+        UserName,
+        Icon,
+        HighlightCards} 
+from './styles';
+
+import { HighlightCard } from '../../components/HighlightCard';
+
+export default function Dashboard() {
+    return (
+        <Container>
+            <Header>
+
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo 
+                            source={{uri: 'https://avatars.githubusercontent.com/u/69051403?v=4'}}
+                        />
+
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+
+                            <UserName>Anthony</UserName>
+                        </User>
+
+                    </UserInfo>
+
+                    <Icon name="power"/>
+                </UserWrapper>
+
+            </Header>
+
+            <HighlightCards>
+                <HighlightCard/>
+                <HighlightCard/>
+                <HighlightCard/>
+            </HighlightCards>
+        </Container>
+    )
+}
